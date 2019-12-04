2eae25b27ada29a98e752beb92bca6dc8cded5a4
Anton Bredenbeck <anton.bredenbeck@stud-mail.uni-wuerzburg.de>
2019-12-04 20:55:02 +0100

    Added Full Framework for controlling pwm signal of Pi GPIO

--- pwm_commander/src/pwm_to_pin.cpp
+++ pwm_commander/src/pwm_to_pin.cpp
@@ -1,10 +1,19 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-
+# include <wiringPi.h>
 #include <stdio.h>
 #include <sstream>
 
 int PWM = 0;
+//Constants
+const int PIN = 12;
+const long BASE_FREQ = 19600000; //19.6*10^6 Hz
+const int RANGE_VALUE = 200;
+const int CLOCK_DIVISOR = 1920;
+
+/*Required Subfunction declaration*/
+void init_motor(int pin_number);
+int percent2ticknumber(double percent_value);
 
 void pwmValCallback(const std_msgs::String::ConstPtr& msg)
 {
@@ -21,15 +30,56 @@ int main(int argc, char **argv)
   */
   ros::init(argc, argv, "pwm_to_pin");
   ros::NodeHandle n;
+
   /*Init Subscriber functionality*/
   ros::Subscriber sub = n.subscribe("pwm_const", 1000, pwmValCallback);
   
+/*Init wiringPi functionality*/
+//WiringPi Vlaues
+wiringPiSetupPhys();
+pwmSetMode(PWM_MODE_MS);
+pwmSetRange(RANGE_VALUE);
+pwmSetClock(CLOCK_DIVISOR);
+pinMode(PIN,PWM_OUTPUT);
+
   while(ros::ok())
   {
-    ROS_INFO("Putting PWM %d on Pin", PWM);
-    //TODO actually putting PWM on pin
-    ros::spinOnce();
+    
+    
+     int cor_value = percent2ticknumber(PWM/100.0);   
+     pwmWrite(PIN,cor_value);
+     ROS_INFO("Putting %d ticks on Pin", cor_value);
+     ros::spinOnce();
   }
 
   return 0;
 }
+
+void init_motor(int pin_number)
+{
+   std::cout << "Duty Cycle = 0%" << std::endl; 
+   pwmWrite(pin_number,percent2ticknumber(0));
+   std::cout << "Connect Motor Now..." << std::endl; 
+   delay(5000);
+   std::cout<< "...Continuing" <<std::endl;
+   
+   std::cout << "Duty Cycle = 100%" << std::endl; 
+   pwmWrite(pin_number,percent2ticknumber(1.0));
+   delay(5000);
+   std::cout<< "...Continuing" <<std::endl;
+   
+   std::cout << "Duty Cycle = 50%" << std::endl; 
+   pwmWrite(pin_number,percent2ticknumber(0.5));
+   delay(5000);
+   std::cout<< "...Continuing" <<std::endl;
+}
+
+int percent2ticknumber(double percent_value)
+{
+  int ticks_per_ms = (BASE_FREQ/CLOCK_DIVISOR)/1000.0;
+  //int tick_number = (ticks_per_ms + percent_value*ticks_per_ms); 
+  //return tick_number;
+  return int(20*ticks_per_ms*percent_value);
+}
+
+
